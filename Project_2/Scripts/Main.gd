76a8dcaa6f38d8b5ca3b89a0f0c7c2a097367ddd
EgorRudenko <47899212+EgorRudenko@users.noreply.github.com>
2021-01-03 19:37:37 +0200

    Add files via upload

--- Project_2/Scripts/Main.gd
+++ Project_2/Scripts/Main.gd
@@ -1,29 +1,42 @@
 extends Spatial
 signal win
+signal lose
 
+#Class with mathematics
 var mathClass = load("res://Scripts/classes/math.gd")
 var math = mathClass.new()
+
 onready var end = get_node("End")
 onready var player = get_node("Player")
 onready var camera = get_node("Camera")
 onready var label = get_node("Camera/Label")
 var isRun = true
 
-		
+
 func _ready():
 	label.visible = false
 
+func _on_win():
+	label.text = "You've won"
+	label.visible = true
+	isRun = false
+
+func _on_lose():
+	label.text = "You lose"
+	label.visible = true
+	isRun = false
+
 func _process(delta):
 	if isRun:
-		var pp = player.translation
-		var pe = end.translation
-		if math.mod(math.mod(pp.z) - math.mod(pe.z)) < 2 and math.mod(math.mod(pp.y)-math.mod(pe.y)) < 2:
-			label.text = "You've won"
-			label.visible = true
-			isRun = false
+		
+		var pp = player.translation #player's position
+		var pe = end.translation #end's position
+		if math.mod(math.mod(pp.z) - math.mod(pe.z)) < 2 and math.mod(math.mod(pp.y)-math.mod(pe.y)) < 2: # If player has colision with end
+			emit_signal("win")
+			
+		if player.translation.y < -10: #lose
+			emit_signal("lose")
+			
 		camera.translation.z = player.translation.z-3
 		camera.translation.y = player.translation.y+3.5
-		if player.translation.y < -10:
-			label.text = "You lose"
-			label.visible = true
-			isRun = false
+
--- Project_2/Scripts/Player.gd
+++ Project_2/Scripts/Player.gd
@@ -1,7 +1,9 @@
 extends KinematicBody
 
+#Class with matematics
 var mathClass = load("res://Scripts/classes/math.gd")
 var math = mathClass.new()
+
 onready var end = get_node("../End")
 onready var player = get_node("../Player")
 onready var vel = Vector3()
@@ -10,13 +12,17 @@ var gravity = 5
 var speed = 1
 var isRun = true
 
+func _on_win():
+	visible = false
+	isRun = false
+	
+func _on_lose():
+	visible = false
+	isRun = false
+
 func _physics_process(delta):
 	if isRun:
-		var pp = player.translation
-		var pe = end.translation
-		if math.mod(math.mod(pp.z) - math.mod(pe.z)) < 2 and math.mod(math.mod(pp.y)-math.mod(pe.y)) < 2:
-			visible = false
-			isRun = false
+		#Control
 		if Input.is_action_pressed("ui_right"):
 			#vel.z -= speed
 			move_and_collide(Vector3(0,0,-0.3))
@@ -27,8 +33,7 @@ func _physics_process(delta):
 			player.rotation_degrees.y = 180
 		if Input.is_action_pressed("ui_up") and is_on_floor():
 			vel.y += jump_speed
-		vel.y -= gravity	
+		
+		#gravity
+		vel.y -= gravity 
 		vel = move_and_slide(vel, Vector3.UP)
-		if translation.y < -10:
-			visible = false
-			isRun = false
